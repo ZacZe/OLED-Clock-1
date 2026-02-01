ca81a996dc552fac158b9adfc3aecf3086e04832
ZacZe <zachariah.zergoug@gmail.com>
2026-02-01 15:25:58 +0000

    Refactored boilerplate code and update README
    Changed boilerplate code to be further simplified.
    README updated for others to try out program.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# oledclock1
+# OLED Clock
 ESP32 + 0.96" OLED screen used to create a mini clock and temperature reader. Reads both data via public APIs. 
+
+0.96" OLED screen is an I2C module. 
+
+### ESP -> OLED
+3V3 -> VCC
+GND -> GND
+D21 -> SDA
+D22 -> SCL
--- oledclock1.ino
+++ oledclock1.ino
@@ -1,14 +1,9 @@
-#include <WiFi.h>
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-const char* ssid = "yourNetworkName";
-const char* password =  "yourNetworkPass";
-
-String temperatureReader = "https://api.open-meteo.com/v1/forecast?latitude=51.5085&longitude=-0.1257&models=ukmo_seamless&current=temperature_2m";
-String time = "http://worldtimeapi.org/api/timezone/Europe/London.txt"
+#define builtin_led 2 
 
 #define SCREEN_WIDTH 128 // width
 #define SCREEN_HEIGHT 64 // height
@@ -17,62 +12,62 @@ String time = "http://worldtimeapi.org/api/timezone/Europe/London.txt"
 #define SCREEN_ADDRESS 0x3C // 0x3C, not 0x3D ever!!!!
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-#define LOGO_HEIGHT   16
-#define LOGO_WIDTH    16
+#define LOGO_HEIGHT 15
+#define LOGO_WIDTH 16
+
+static const unsigned char PROGMEM logo_bmp[] =
+{ 0b11111111, 0b11111111,
+  0b11111111, 0b11111111,
+  0b11111111, 0b11111111,
+  0b00000000, 0b11100111,
+  0b00000001, 0b11001110,
+  0b00000011, 0b10011100,
+  0b00000111, 0b00111000,
+  0b00001110, 0b01110000,
+  0b00011100, 0b11100000,
+  0b00111001, 0b11000000,
+  0b01110011, 0b10000000,
+  0b11100111, 0b00000000,
+  0b11111111, 0b11111111,
+  0b11111111, 0b11111111,
+  0b11111111, 0b11111111 
+};
 
 void setup() {
-  // put your setup code here, to run once:
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
-    drawConnecting();
+  Serial.begin(9600);
+  Wire.begin(21, 22);                  // explicitly init I2C
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 failed"));
+    while(1);
   }
 
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+  display.println(F("BOOT OK"));
+  display.setCursor(2,0);
+  display.println(F("I/O OK"));
+  display.display();
 
+  drawLogo(); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.println(F("Hello, World!"));
+  display.display();
+  delay(500);
 }
 
-void drawLocationTmp(void) {
+void drawLogo() {
   display.clearDisplay();
 
-  display.setTextSize(1);             // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE);        // Draw white text
-  display.setCursor(0,0);             // Start at top-left corner
-  display.println(F("Location: "));
-  display.println(F("");)
-
+  display.drawBitmap(
+    (display.width()  - LOGO_WIDTH ) / 2,
+    (display.height() - LOGO_HEIGHT) / 2,
+    logo_bmp, LOGO_WIDTH, LOGO_HEIGHT, 1);
   display.display();
   delay(2000);
-}
-
-void drawConnecting(void) {
-  display.clearDisplay();
-
-  display.setTextSize(1);             // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE);        // Draw white text
-  display.setCursor(0,0);             // Start at top-left corner
-  display.println(F("Connecting to the internet"));
-  display.println(F(".");)
-  display.display();
-  delay(1000)
-  display.println(F(".");)
-  display.display();
-  delay(1000)
-  display.println(F(".");)
-  display.display();
-}
-
-void drawTime(void) {
-  display.clearDisplay(); 
-  display.setTextSize(3); 
-  display.setTextColor(SSD1306_WHITE);        // Draw white text
-  display.setCursor(4,0);
-  display.println(F(time));
-  display.display();
-  delay(1000); 
 }
